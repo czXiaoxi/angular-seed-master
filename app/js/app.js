dcb099045969fda753267a6058db17329f54abc3
王希 <zona_xi@163.com>
2015-09-13 01:10:24 +0800

    cm

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,19 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+//angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+//  config(['$routeProvider', function($routeProvider) {
+//    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
+//    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
+//    $routeProvider.otherwise({redirectTo: '/view1'});
+//  }]);
+//'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('jthink', ['jthink.filters', 'jthink.services', 'jthink.directives']).
+    config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: MyCtrl2});
+      $routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: MyCtrl2});
+      $routeProvider.otherwise({redirectTo: '/login'});
+    }]);
